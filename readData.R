de092a9e518fe56f7ddb04454b414e8d0345c41c
richarddmorey <richarddmorey@gmail.com>
2016-04-05 09:34:59 +0100

    added content

---
+++ readData.R
@@ -0,0 +1,5 @@
+# Read from GitHub 
+ci.data = read.csv("https://raw.githubusercontent.com/richarddmorey/openScienceExample/master/HoekstraEtAl2014.data.csv")
+
+# Read from local file
+# ci.data = read.csv("HoekstraEtAl2014.data.csv")
